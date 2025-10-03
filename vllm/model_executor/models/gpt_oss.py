113770330403881c5080113a5ad02620871fa70a
Danielle Robinson <dmmaddix@amazon.com>
2025-10-03 00:03:15 +0000

    remove bias error from fused moe modular kernel to test unquantized gpt-oss base model

--- vllm/model_executor/models/gpt_oss.py
+++ vllm/model_executor/models/gpt_oss.py
@@ -157,7 +157,7 @@ class MLPBlock(torch.nn.Module):
                                 quant_config=quant_config,
                                 prefix=f"{prefix}.experts",
                                 apply_router_weight_on_input=False,
-                                has_bias=True,
+                                has_bias=False, #FIXME
                                 activation="swigluoai")
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -633,9 +633,9 @@ class GptOssForCausalLM(nn.Module, SupportsPP, MixtureOfExperts, SupportsLoRA):
             ".gate_up_proj": ".w13_weight",
             ".down_proj": ".w2_weight",
 
-            # MoE Bias
-            ".gate_up_proj_bias": ".w13_bias",
-            ".down_proj_bias": ".w2_bias",
+            # # MoE Bias
+            # ".gate_up_proj_bias": ".w13_bias",
+            # ".down_proj_bias": ".w2_bias",
         },
     )
 
